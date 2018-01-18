ef427eec8fc299ee7bf2c85f03c5775b5fab23dd
Jean <spyroo913@hotmail.com>
2018-01-18 16:20:12 +0100

    add story work

--- app/src/main/java/groupe_9/com/fabellas/StoriesListActivity.java
+++ app/src/main/java/groupe_9/com/fabellas/StoriesListActivity.java
@@ -2,6 +2,7 @@ package groupe_9.com.fabellas;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -12,6 +13,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -198,9 +200,10 @@ public class StoriesListActivity extends AppCompatActivity implements View.OnCli
     public void onClick(View v) {
         if (v.getId() == R.id.icon)
         {
-            Log.i(MapActivity.TAG, String.format("PLS PLS PLS PLS PLS PLS PLS PLS PLS PLS PLS PLS PLS PLS PLS PLS PLS PLS "));
-            Intent intent = new Intent(this,AddStorieActivity.class);
-            startActivityForResult(intent, REQUEST_CODE_FOR_ADD_STORIE_ACTIVITY);
+            if(!FirebaseAuth.getInstance().getCurrentUser().isAnonymous()) {
+                Intent intent = new Intent(this, AddStorieActivity.class);
+                startActivityForResult(intent, REQUEST_CODE_FOR_ADD_STORIE_ACTIVITY);
+            }
         }
     }
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -209,11 +212,18 @@ public class StoriesListActivity extends AppCompatActivity implements View.OnCli
         {
             String title = data.getStringExtra(AddStorieActivity.TITLE_VALUE);
             String details = data.getStringExtra(AddStorieActivity.DETAILS_VALUE);
-            Toast.makeText(this,"title :  => " + title + "\nDetails : " + details,Toast.LENGTH_SHORT).show();
-
+            addNewStory(title, details);
         }
-
     }
+
+    private void addNewStory(String title, String details){
+        String userUid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        DatabaseReference StoryDatabaseReference = FirebaseDatabase.getInstance().getReference("Stories").push();
+        StoryDatabaseReference.setValue(new Story(StoryDatabaseReference.getKey(), details, id, title, userUid));
+        mDatabaseReference.push().setValue(StoryDatabaseReference.getKey());
+        FirebaseDatabase.getInstance().getReference("Users").child(userUid).child("stories").push().setValue(StoryDatabaseReference.getKey());
+    }
+
     @Override
     public void onPointerCaptureChanged(boolean hasCapture) {
 
