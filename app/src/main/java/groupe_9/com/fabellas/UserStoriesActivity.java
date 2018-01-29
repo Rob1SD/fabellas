d01ce5d941d84cb2557cf69ae7dad2429670f344
Jean <spyroo913@hotmail.com>
2018-01-30 00:32:18 +0100

    suppression ok (error in onbindviewholder)

--- app/src/main/java/groupe_9/com/fabellas/UserStoriesActivity.java
+++ app/src/main/java/groupe_9/com/fabellas/UserStoriesActivity.java
@@ -1,12 +1,24 @@
 package groupe_9.com.fabellas;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.annotation.Nullable;
 import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.GenericTypeIndicator;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 import groupe_9.com.fabellas.bo.Story;
+import groupe_9.com.fabellas.firebase.Utils;
+import groupe_9.com.fabellas.fragments.YesNoDialogFragment;
 import groupe_9.com.fabellas.utils.OnStoryClickable;
 
 /**
@@ -16,6 +28,8 @@ import groupe_9.com.fabellas.utils.OnStoryClickable;
 public class UserStoriesActivity
         extends StoriesListActivity implements OnStoryClickable
 {
+    public static final String BUNDLE_STORY = "bundleStory";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState)
     {
@@ -55,6 +69,43 @@ public class UserStoriesActivity
     @Override
     public void clickedOnStory(Story story)
     {
+        Bundle bundle = new Bundle();
+        bundle.putSerializable(BUNDLE_STORY, story);
+        YesNoDialogFragment yesNoDialogFragment = new YesNoDialogFragment();
+        yesNoDialogFragment.setArguments(bundle);
+        yesNoDialogFragment.show(getFragmentManager(), "YesNoDialog");
 
     }
+
+    public void deleteClickedStory(Story story){
+        final DatabaseReference mPlaceStoriesDatabaseReference =
+                Utils.getDatabase().getReference("Places").child(story.getPlaceId()).child("stories");
+        final DatabaseReference mUserStoriesDatabaseReference =
+                Utils.getDatabase().getReference("Users").child(story.getUserId()).child("stories");
+
+        mPlaceStoriesDatabaseReference.addListenerForSingleValueEvent(getStoriesValueListener(story.getUID()));
+        mUserStoriesDatabaseReference.addListenerForSingleValueEvent(getStoriesValueListener(story.getUID()));
+
+        Utils.getDatabase().getReference("Stories").child(story.getUID()).removeValue();
+    }
+
+    private ValueEventListener getStoriesValueListener(String storyId){
+        return new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {;
+                for(DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    if(storyId.equals(snapshot.getValue())){
+                        snapshot.getRef().removeValue();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        };
+    }
+
+
 }
---
+++ app/src/main/java/groupe_9/com/fabellas/fragments/YesNoDialogFragment.java
@@ -0,0 +1,42 @@
+package groupe_9.com.fabellas.fragments;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+
+import groupe_9.com.fabellas.UserStoriesActivity;
+import groupe_9.com.fabellas.bo.Story;
+import groupe_9.com.fabellas.bo.User;
+
+/**
+ * Created by spyro on 29/01/2018.
+ */
+
+public class YesNoDialogFragment extends DialogFragment {
+
+
+    public YesNoDialogFragment() {
+
+    }
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(getActivity());
+        alertDialogBuilder.setTitle("Confirmation");
+        alertDialogBuilder.setMessage("Etes-vous sûr de vouloir supprimer cette anecdote ?");
+        //null should be your on click listener
+        alertDialogBuilder.setPositiveButton("OUI", (dialog, i) -> {
+            UserStoriesActivity callingActivity = (UserStoriesActivity) getActivity();
+            Bundle bundle = getArguments();
+            Story clickedStory = (Story)bundle.getSerializable(UserStoriesActivity.BUNDLE_STORY);
+            callingActivity.deleteClickedStory(clickedStory);
+            dialog.dismiss();
+        });
+        alertDialogBuilder.setNegativeButton("NON", (dialog, which) -> dialog.dismiss());
+
+
+        return alertDialogBuilder.create();
+    }
+}
